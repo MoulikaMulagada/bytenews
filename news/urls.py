59ad3768eeffe9e9e77434e419a32f422bc5519a
MoulikaMulagada <moulikamulagada13@gmail.com>
2025-07-24 23:59:23 +0530

    Add files via upload

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import reading_history_view
 
 app_name = 'news'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.ArticleListView.as_view(), name='article_list'),
-    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='detail'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', views.home, name='home'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('reading-history/', reading_history_view, name='reading_history'),  # ✅ function-based view
+    path('', views.ArticleListView.as_view(), name='home'),
+
+    
 ]
